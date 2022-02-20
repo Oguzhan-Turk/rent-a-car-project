09701947f99a1ceab00236e70bb54eaec2d1055e
Oguzhan-Turk <oguzhan.turkk@hotmail.com>
2022-02-20 23:10:01 +0300

    created car dao

---
+++ rentacar/src/main/java/com/oguzhanturk/rentacar/dataAccess/abstracts/CarDao.java
@@ -0,0 +1,12 @@
+package com.oguzhanturk.rentacar.dataAccess.abstracts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.oguzhanturk.rentacar.entities.concretes.Car;
+
+@Repository
+public interface CarDao extends JpaRepository<Car, Integer> {
+
+//	Car getByCarId(int id);
+}
