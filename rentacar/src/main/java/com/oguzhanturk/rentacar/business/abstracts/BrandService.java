cbe4ca2abc3d559782b076b1d1a7db76986a69dd
Oguzhan-Turk <oguzhan.turkk@hotmail.com>
2022-02-20 23:06:50 +0300

    update services

--- rentacar/src/main/java/com/oguzhanturk/rentacar/business/abstracts/BrandService.java
+++ rentacar/src/main/java/com/oguzhanturk/rentacar/business/abstracts/BrandService.java
@@ -2,15 +2,22 @@ package com.oguzhanturk.rentacar.business.abstracts;
 
 import java.util.List;
 
+import com.oguzhanturk.rentacar.business.dtos.BrandDto;
 import com.oguzhanturk.rentacar.business.dtos.ListBrandDto;
 import com.oguzhanturk.rentacar.business.request.CreateBrandRequest;
+import com.oguzhanturk.rentacar.business.request.DeleteBrandRequest;
+import com.oguzhanturk.rentacar.business.request.UpdateBrandRequest;
 
 public interface BrandService {
 
-	List<ListBrandDto> getAll();
-
 	void add(CreateBrandRequest createBrandRequest);
 
-	ListBrandDto getById(int id);
+	void update(UpdateBrandRequest updateBrandRequest);
+
+	void delete(DeleteBrandRequest deleteBrandRequest);
+
+	List<ListBrandDto> getAll();
+
+	BrandDto getById(int id);
 
 }
--- rentacar/src/main/java/com/oguzhanturk/rentacar/business/abstracts/ColorService.java
+++ rentacar/src/main/java/com/oguzhanturk/rentacar/business/abstracts/ColorService.java
@@ -2,14 +2,22 @@ package com.oguzhanturk.rentacar.business.abstracts;
 
 import java.util.List;
 
+import com.oguzhanturk.rentacar.business.dtos.ColorDto;
 import com.oguzhanturk.rentacar.business.dtos.ListColorDto;
 import com.oguzhanturk.rentacar.business.request.CreateColorRequest;
+import com.oguzhanturk.rentacar.business.request.DeleteColorRequest;
+import com.oguzhanturk.rentacar.business.request.UpdateColorRequest;
 
 public interface ColorService {
 
 	List<ListColorDto> getAll();
 
-	void add(CreateColorRequest createColorRequest);
+	ColorDto getById(int id);
 
-	ListColorDto getById(int id);
+	ColorDto add(CreateColorRequest createColorRequest);
+
+	void delete(DeleteColorRequest deleteColorRequest);
+	
+	void update(UpdateColorRequest updateColorRequest);
+
 }
