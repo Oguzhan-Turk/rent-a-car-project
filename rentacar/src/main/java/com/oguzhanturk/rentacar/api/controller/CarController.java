ab472b1e6d4cbf1b79d11c56a35a6eca658dacb7
Oguzhan-Turk <oguzhan.turkk@hotmail.com>
2022-03-03 14:30:17 +0300

    removed unnecessary imports

--- rentacar/src/main/java/com/oguzhanturk/rentacar/api/controller/CarController.java
+++ rentacar/src/main/java/com/oguzhanturk/rentacar/api/controller/CarController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
