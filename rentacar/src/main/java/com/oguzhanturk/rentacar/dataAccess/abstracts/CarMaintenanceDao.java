d8814296182b6d49e522451f685b1f83ed579656
Oguzhan-Turk <oguzhan.turkk@hotmail.com>
2022-03-06 02:56:30 +0300

    create CarMaintenanceDao

---
+++ rentacar/src/main/java/com/oguzhanturk/rentacar/dataAccess/abstracts/CarMaintenanceDao.java
@@ -0,0 +1,14 @@
+package com.oguzhanturk.rentacar.dataAccess.abstracts;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.oguzhanturk.rentacar.entities.concretes.CarMaintenance;
+
+@Repository
+public interface CarMaintenanceDao extends JpaRepository<CarMaintenance, Integer> {
+
+	List<CarMaintenance> getAllByCarCarId(Integer id);
+}
