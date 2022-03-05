b27ab1e5496f47a1d7fae74e3343fa726afee24e
Oguzhan-Turk <oguzhan.turkk@hotmail.com>
2022-03-06 02:56:12 +0300

    created CarMaintenance Entity

---
+++ rentacar/src/main/java/com/oguzhanturk/rentacar/entities/concretes/CarMaintenance.java
@@ -0,0 +1,37 @@
+package com.oguzhanturk.rentacar.entities.concretes;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "car_maintenances")
+@Entity
+public class CarMaintenance {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "maintenance_id")
+	private int maintenanceId;
+	@Column(name = "maintenance_description")
+	private String maintenanceDescription;
+	@Column(name = "return_date")
+	private LocalDateTime returnDate;
+	@ManyToOne
+	@JoinColumn(name = "maintenance_car_id")
+	private Car car;
+
+}
