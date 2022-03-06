08eb953ddfec5130cb90df1e83aa092a38f03d21
Oguzhan-Turk <oguzhan.turkk@hotmail.com>
2022-03-07 00:12:01 +0300

    created rental entity

---
+++ rentacar/src/main/java/com/oguzhanturk/rentacar/entities/concretes/Rental.java
@@ -0,0 +1,45 @@
+package com.oguzhanturk.rentacar.entities.concretes;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "rentals")
+@Entity
+public class Rental {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "rent_id")
+	private int rentId;
+
+	@Column(name = "rent_date")
+	private LocalDateTime rentDate;
+
+	@Column(name = "return_date")
+	private LocalDateTime returnDate;
+
+	@OneToOne
+	@JoinColumn(name = "car_id")
+	private Car car;
+
+	@ManyToOne
+	@JoinColumn(name = "customer_id")
+	private Customer customer;
+
+}
