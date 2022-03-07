c98f39fce3a0f0fd8a3dbf6527f266410e467d1e
Oguzhan-Turk <oguzhan.turkk@hotmail.com>
2022-03-07 16:59:12 +0300

    added exception handler

--- rentacar/src/main/java/com/oguzhanturk/rentacar/RentacarApplication.java
+++ rentacar/src/main/java/com/oguzhanturk/rentacar/RentacarApplication.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import com.oguzhanturk.rentacar.core.utilities.exceptions.BusinessException;
 import com.oguzhanturk.rentacar.core.utilities.results.ErrorDataResult;
 
 @RestControllerAdvice
@@ -28,16 +29,26 @@ public class RentacarApplication {
 	public ModelMapper getModelMapper() {
 		return new ModelMapper();
 	}
-	
+
 	@ExceptionHandler
 	@ResponseStatus(code = HttpStatus.BAD_REQUEST)
-	public ErrorDataResult<Object> handleValidationExceptions(MethodArgumentNotValidException argumentNotValidException) {
+	public ErrorDataResult<Object> handleValidationExceptions(
+			MethodArgumentNotValidException argumentNotValidException) {
 		Map<String, String> validationErrors = new HashMap<String, String>();
 		for (FieldError fieldError : argumentNotValidException.getBindingResult().getFieldErrors()) {
 			validationErrors.put(fieldError.getField(), fieldError.getDefaultMessage());
 		}
 		ErrorDataResult<Object> errorDataResult = new ErrorDataResult<Object>(validationErrors, "Validation.Error");
 		return errorDataResult;
+	}
+
+	@ExceptionHandler
+	@ResponseStatus(code = HttpStatus.BAD_REQUEST)
+	public ErrorDataResult<Object> handleBusinessExceptions(BusinessException businessException) {
+		ErrorDataResult<Object> errorDataResult = new ErrorDataResult<Object>(businessException.getMessage(),
+				"Business.Exception.Errors");
+		return errorDataResult;
+
 	}
 
 }
