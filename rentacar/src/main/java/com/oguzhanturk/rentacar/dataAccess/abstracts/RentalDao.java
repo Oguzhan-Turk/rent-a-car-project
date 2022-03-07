f4130a74f4564af2c35f27e5a599ffc3a45ab8fe
Oguzhan-Turk <oguzhan.turkk@hotmail.com>
2022-03-07 14:33:40 +0300

    create RentalDao

---
+++ rentacar/src/main/java/com/oguzhanturk/rentacar/dataAccess/abstracts/RentalDao.java
@@ -0,0 +1,11 @@
+package com.oguzhanturk.rentacar.dataAccess.abstracts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.oguzhanturk.rentacar.entities.concretes.Rental;
+
+@Repository
+public interface RentalDao extends JpaRepository<Rental, Integer> {
+
+}
