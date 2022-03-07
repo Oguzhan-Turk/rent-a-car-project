4a5da425431efc36f902319013f629907cbc353b
Oguzhan-Turk <oguzhan.turkk@hotmail.com>
2022-03-07 14:39:34 +0300

    create rental dtos

---
+++ rentacar/src/main/java/com/oguzhanturk/rentacar/business/dtos/RentalDto.java
@@ -0,0 +1,25 @@
+package com.oguzhanturk.rentacar.business.dtos;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RentalDto {
+
+	private int rentId;
+	private LocalDateTime rentDate;
+	private LocalDateTime returnDate;
+
+	private int carId;
+	private String brandName;
+	private int modelYear;
+	private String description;
+	private BigDecimal dailyPrice;
+
+}
